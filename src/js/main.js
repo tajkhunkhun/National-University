31d00ce23e8e3be32ea2ef83224f43f0b606f2f3
tajkhunkhun <khunkhuntaj@gmail.com>
2025-08-20 14:52:12 +0300

    added mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,72 @@
 document.addEventListener('DOMContentLoaded', () => {
+	// Mobile menu
+	const mobileMenu = document.querySelector('.mobile-menu')
+	const mobileMenuButton = document.querySelector('.mobile-menu-button')
+	const mobileMenuOverlay = document.querySelector('.mobile-menu-overlay')
+
+	mobileMenuButton.addEventListener('click', () => {
+		const isActive = mobileMenu.classList.contains('active-menu')
+		const mobileMenuHeight = mobileMenu.scrollHeight
+		console.log(mobileMenuHeight)
+		if (!isActive) {
+			mobileMenu.classList.add('active-menu')
+			mobileMenuButton.classList.add('active-menu')
+			mobileMenu.style.maxHeight = `${mobileMenuHeight}px`
+			mobileMenuOverlay.classList.add('active-menu')
+		} else {
+			mobileMenu.classList.remove('active-menu')
+			mobileMenuButton.classList.remove('active-menu')
+			mobileMenu.style.maxHeight = '0px'
+			mobileMenuOverlay.classList.remove('active-menu')
+		}
+	})
+
+	document.addEventListener('click', e => {
+		if (
+			!mobileMenu.contains(e.target) &&
+			!mobileMenuButton.contains(e.target)
+		) {
+			mobileMenu.classList.remove('active-menu')
+			mobileMenuButton.classList.remove('active-menu')
+			mobileMenu.style.maxHeight = '0px'
+			mobileMenuOverlay.classList.remove('active-menu')
+		}
+	})
+
 	// Header scroll logic
 	const header = document.querySelector('header')
 	let lastScrollTop = 0
 	let scrollThreshold = 100
-	let isScrolling = false
-	let scrollTimeout
+	let isActive = false
+	let minScrollDistance = 5
 
 	function handleScroll() {
 		const currentScrollTop =
 			window.pageYOffset || document.documentElement.scrollTop
 
+		const scrollDistance = Math.abs(currentScrollTop - lastScrollTop)
+
 		if (currentScrollTop > scrollThreshold) {
-			if (currentScrollTop > lastScrollTop) {
+			if (
+				currentScrollTop > lastScrollTop &&
+				scrollDistance > minScrollDistance &&
+				!isActive
+			) {
 				header.classList.add('active')
-			} else if (currentScrollTop < lastScrollTop - 10) {
+				isActive = true
+			} else if (
+				currentScrollTop < lastScrollTop &&
+				scrollDistance > minScrollDistance &&
+				isActive
+			) {
 				header.classList.remove('active')
+				isActive = false
 			}
 		} else {
-			header.classList.remove('active')
+			if (isActive) {
+				header.classList.remove('active')
+				isActive = false
+			}
 		}
 
 		lastScrollTop = currentScrollTop
